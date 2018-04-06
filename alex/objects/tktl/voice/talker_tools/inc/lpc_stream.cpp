fa4dbdbf0849536cf982c3df9743b844c880ddf1
alx <team_talker@protonmail.com>
2018-04-07 00:42:10 +0100

    Reader Freeze Tweaks

--- alex/objects/tktl/voice/talker_tools/inc/lpc_stream.cpp
+++ alex/objects/tktl/voice/talker_tools/inc/lpc_stream.cpp
@@ -1,66 +1,70 @@
 //
 //  lpc_stream.cpp
-//  C++ Tests
-//
-//  Created by alx on 21/03/2018.
-//  Copyright © 2018 alx. All rights reserved.
 //
 
-/////////////////
-// LPC indices //
-/////////////////
+/////////////////////
+/////////////////////
+//// LPC indices ////
+/////////////////////
+/////////////////////
 
+// Constructor
 TktlLpcIndices::TktlLpcIndices() {
-    Silence();
+	is_reading_        = false;
+	word_end_trigger_  = false;
+	frame_trigger_     = false;
+	is_unvoiced_frame_ = false;
+	is_rest_frame_     = false;
+	is_repeat_frame_   = false;
+	SetSilent();
 };
 
 // Zero all LPC values
-void TktlLpcIndices::Silence() {
+void TktlLpcIndices::SetSilent() {
     for(int i = 0; i < 12; i++) {
         indices_[i] = 0;
     };
 };
 
 // Zero K values for unvoiced frame
-void TktlLpcIndices::Unvoiced() {
+void TktlLpcIndices::SetUnvoiced() {
     for(int i = 6; i < 12; i++) {
         indices_[i] = 0;
     };
 };
 
-/////////////////////////////
-// Point in LPC datastream //
-/////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+//// Point in LPC datastream ////
+/////////////////////////////////
+/////////////////////////////////
 
 TktlLpcStreamPosition::TktlLpcStreamPosition() {
 	word_start_byte_ = NULL;
-    byte_ = NULL;
-    bit_ = 0;
-    fragment_ = 0;    
-    saved_byte_ = NULL;
-    saved_bit_ = 0;
-    saved_fragment_ = 0;
+    byte_            = NULL;
+    bit_             = 0;
+    fragment_        = 0;    
+    saved_byte_      = NULL;
+    saved_bit_       = 0;
+    saved_fragment_  = 0;
 };
 
-// Set word-start
+// Set new word-start position
 void TktlLpcStreamPosition::SetWordStart(uint8_t *byte) {
     word_start_byte_ = byte;
-    byte_ = byte;
-    bit_ = 0;
+};
+
+// Jump playhead to word-start
+void TktlLpcStreamPosition::SetToWordStart() {
+	byte_     = word_start_byte_;
+    bit_      = 0;
     fragment_ = 0;
 };
 
-// Reset to start of word (ie for loop)
-void TktlLpcStreamPosition::ResetToWordStart() {
-	byte_ = word_start_byte_;
-	bit_ = 0;
-	fragment_ = 0;
-};
-
-// Set to point
+// Set playhead to arbitrary point
 void TktlLpcStreamPosition::SetToPosition(uint8_t *byte, uint8_t bit, uint8_t fragment) {
-    byte_ = byte;
-    bit_ = bit;
+    byte_     = byte;
+    bit_      = bit;
     fragment_ = fragment;
 };
 
@@ -78,55 +82,117 @@ void TktlLpcStreamPosition::SetToSavedPosition() {
 	fragment_ = saved_fragment_;
 };
     
-////////////////////////////////////
-// Get frame from LPC data stream //
-////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+//// Get frame from LPC data stream ////
+////////////////////////////////////////
+////////////////////////////////////////
+
+////////////////////
+// Public Methods //
+////////////////////
 
 // Constructor
 TktlGetLPCFrame::TktlGetLPCFrame() {
-    read_word_      = true;
-    ptr_word_start_ = NULL;
-    frame_loop_     = false;
-    frame_glitch_   = false;
-    frame_repeat_ 	= 0;
-    frame_energy_ 	= 1;
-	word_end_       = false;
+    read_word_           = true;
+    gate_                = false;
+    ptr_word_start_      = NULL;
+    word_changed_        = true;
+    loop_                = false;
+    freeze_              = false;
+    glitch_              = false;
+    pitch_bits_		     = 6;	// Default VSM2
+    frame_repeat_ 	     = 0;
+    frame_energy_ 	     = 1;	// Needs to be more than 0, or frames will not play
+	word_end_            = false;
+	counter_		     = 0;
+	frame_length_        = 75;	// 25ms default
+	step_frame_          = false;
+	// Zero indices in lpc_indices instances
+	lpc_indices_.SetSilent();
+	//lpc_indices_silent_.SetSilent();
 };
 
-// Get frame
-TktlLpcIndices TktlGetLPCFrame::GetFrame() {
-    // Reset word-end
-    word_end_ = false;
-    ReadFrame();
-    return lpc_indices_;
+// Set pitch bit count
+void TktlGetLPCFrame::SetPitchBits(uint8_t bits = 6) {
+	pitch_bits_ = bits;
 };
 
-// Set word-start pointer
-void TktlGetLPCFrame::StartWord(uint8_t *ptr) {
-    head_.SetWordStart(ptr);
-    read_word_ = true;
-	frame_energy_ = 1;
+// Set word start pointer to start of new word LPC data
+// NOTE: doesn't actually set playhead to start of word
+// This allows word to play to end before new word starts
+void TktlGetLPCFrame::SetWordStart(uint8_t *ptr) {
+	// Update word-start pointer if changed
+	if(ptr_word_start_ != ptr) {
+		head_.SetWordStart(ptr);
+		word_changed_ = true;
+		ptr_word_start_ = ptr;
+	};
+};
+
+// Set gate on/off
+void TktlGetLPCFrame::SetGate(bool gate, bool oneshot = false) {
+	if(gate) {
+		if(!gate_) {
+			// RISING EDGE
+			StartReading();
+		};
+	} else {
+		if(gate_) {
+			// FALLING EDGE
+			// Stop playback only if in gated playback mode
+			if(!oneshot)
+				StopReading();
+		};
+	};
+	gate_ = gate;
+};
+
+// Begin playback from start of selected word
+void TktlGetLPCFrame::StartReading() {
+	//LogTextMessage("Word changed: %d", word_changed_);
+    // Do not set playhead to word start if freeze enabled
+    if(freeze_) {
+		if(word_changed_) {
+			head_.SetToWordStart();
+			ReadFrame();
+			word_changed_ = false;
+		};
+	} else {
+		head_.SetToWordStart();
+	};
+	// Reset counter and flags
+	counter_                 = 0;
+	read_word_               = true;
+	lpc_indices_.is_reading_ = true;
+};
+
+// Stop reading frame data
+void TktlGetLPCFrame::StopReading() {
+	read_word_ = false;
 };
 
 // Set loop
 void TktlGetLPCFrame::SetLoop(bool loop) {
-    frame_loop_ = loop;
+    loop_ = loop;
 };
 
 // Set glitch
 void TktlGetLPCFrame::SetGlitch(bool glitch) {
-	frame_glitch_ = glitch;
+	glitch_ = glitch;
 };
 
-// Stop reading frame data
-void TktlGetLPCFrame::StopRead() {
-    read_word_ = false;
-    lpc_indices_.Silence();
+// Set freeze
+void TktlGetLPCFrame::SetFreeze(bool freeze) {
+	freeze_ = freeze;
 };
 
-// Check for word-end
-bool TktlGetLPCFrame::CheckForWordEnd() {
-	return word_end_;
+// Step to next frame when in freeze mode
+void TktlGetLPCFrame::StepFrozenFrame() {
+	if(freeze_) {
+		ReadFrame();
+		counter_ = 0;
+	};
 };
 
 // Set jump-point
@@ -139,54 +205,101 @@ void TktlGetLPCFrame::SetToJumpPoint() {
 	head_.SetToSavedPosition();
 };
 
+// Set frame length
+void TktlGetLPCFrame::SetFrameLength(uint32_t length) {
+	frame_length_ = length;
+};
+
+// Tick object (call every K-Rate cycle)
+TktlLpcIndices TktlGetLPCFrame::Tick() {		
+	// Reset word-end trigger, frame trigger
+	lpc_indices_.word_end_trigger_ = false;	
+	lpc_indices_.frame_trigger_    = false;
+	// Read frame if counter = 0 and read_word_ true
+	if(counter_ == 0) {			
+		if(read_word_) {
+			// Only update frame if freeze not enabled
+			if(!freeze_) {
+				ReadFrame();
+			};
+			// Send frame-update trigger pulse
+			lpc_indices_.frame_trigger_ = true;
+		} else {
+			lpc_indices_.is_reading_ = false;
+		};
+	};	
+	
+	// Update counter (if freeze true, counter will keep incrementing forever)
+	counter_ = (counter_ < frame_length_) ? counter_ + 1 : 0;
+	// Bit-shift pitch indices up by one, if there are only 5 bits for pitch
+	if(pitch_bits_ == 5)
+    	lpc_indices_.indices_[1] <<= 1;
+    // Return object
+	return lpc_indices_;
+};
+
+/////////////////////
+// Private Methods //
+/////////////////////
+
 // Read word
 void TktlGetLPCFrame::ReadFrame() {
-    // Speak if end of word not reached
-    if(frame_energy_ != 0xf) {
-        frame_energy_ = GetBits(4);
-        if (frame_energy_ == 0) {
-            // Energy = 0: rest frame
-            lpc_indices_.indices_[0] = 0;
-        } else if (frame_energy_ == 0xf) {
-            // Energy = 15: stop frame
-            if(frame_loop_) {
-                head_.ResetToWordStart();
-                frame_energy_ = 1;
-            } else {
-                lpc_indices_.Silence();
-            };
-            // Trigger word-end pulse
-            word_end_ = true;
-        } else {
-            // Get Energy index
-            lpc_indices_.indices_[0] = frame_energy_;
-            // Get Repeat bit
-            frame_repeat_ = GetBits(1);
-            // Get Pitch index
-            lpc_indices_.indices_[1] = GetBits(6);
-            // Get K indices
-            // (Repeat frame uses the last coefficients)
-            if (!frame_repeat_) {
-                // All frames use the first 4 coefficients
-                lpc_indices_.indices_[2] = GetBits(5);
-                lpc_indices_.indices_[3] = GetBits(5);
-                lpc_indices_.indices_[4] = GetBits(4);
-                lpc_indices_.indices_[5] = GetBits(4);
-                if (lpc_indices_.indices_[1]) {
-                    // Voiced frames use 6 extra coefficients.
-                    lpc_indices_.indices_[6]  = GetBits(4);
-                    lpc_indices_.indices_[7]  = GetBits(4);
-                    lpc_indices_.indices_[8]  = GetBits(4);
-                    lpc_indices_.indices_[9]  = GetBits(3);
-                    lpc_indices_.indices_[10] = GetBits(3);
-                    lpc_indices_.indices_[11] = GetBits(3);
-                } else {
-                    // Zero last 6 coefficients for unvoiced frames
-                    lpc_indices_.Unvoiced();
-                };
-            };
-        };
-    };
+    // Reset Rest, Unvoiced frame flags
+    lpc_indices_.is_rest_frame_     = false;
+    lpc_indices_.is_unvoiced_frame_ = false;
+    lpc_indices_.is_repeat_frame_   = false;
+      
+	frame_energy_ = GetBits(4);
+	if (frame_energy_ == 0) {
+		// Energy = 0: rest frame
+		lpc_indices_.SetSilent();
+		 // Set Rest flag in lpc_indices_ instance
+		lpc_indices_.is_rest_frame_ = true;
+	} else if (frame_energy_ == 15) {
+		// Energy = 15: stop frame            
+		// If loop true, reset playhead to beginning of word
+		// Also reset if freeze set
+		if((loop_) || (freeze_)) {
+			head_.SetToWordStart();
+		} else {
+			read_word_ = false;
+			lpc_indices_.SetSilent();
+		};
+		// Trigger word-end pulse
+		lpc_indices_.word_end_trigger_ = true;
+	} else {
+		// Get Energy index
+		lpc_indices_.indices_[0] = frame_energy_;
+		// Get Repeat bit
+		frame_repeat_ = GetBits(1);
+		// Get Pitch index
+		lpc_indices_.indices_[1] = GetBits(6);
+		// Get K indices
+		// (Repeat frame uses the last coefficients)
+		if(frame_repeat_) {
+			lpc_indices_.is_repeat_frame_ = true;
+		} else {
+			// All frames use the first 4 coefficients
+			lpc_indices_.indices_[2] = GetBits(5);
+			lpc_indices_.indices_[3] = GetBits(5);
+			lpc_indices_.indices_[4] = GetBits(4);
+			lpc_indices_.indices_[5] = GetBits(4);
+			if (lpc_indices_.indices_[1]) {
+				// Voiced frames use 6 extra coefficients.
+				lpc_indices_.indices_[6]  = GetBits(4);
+				lpc_indices_.indices_[7]  = GetBits(4);
+				lpc_indices_.indices_[8]  = GetBits(4);
+				lpc_indices_.indices_[9]  = GetBits(3);
+				lpc_indices_.indices_[10] = GetBits(3);
+				lpc_indices_.indices_[11] = GetBits(3);
+			} else {
+				// Zero last 6 coefficients for unvoiced frames
+				lpc_indices_.SetUnvoiced();
+				// Set Unvoiced flag in lpc_indices_ instance
+				lpc_indices_.is_unvoiced_frame_ = true;
+			};
+		};
+	};
 };
 
 // Read bits from ROM
@@ -211,7 +324,7 @@ uint8_t TktlGetLPCFrame::GetBits(uint8_t bits) {
 uint8_t TktlGetLPCFrame::ReverseBits(uint8_t byte) {
     // Don't reverse bits if glitch flag set
     // Maybe there's a better way to do a temporary glitch?
-    if(frame_glitch_) {
+    if(glitch_) {
     	return byte;
     } else {
     	uint32_t r = byte;
@@ -219,6 +332,7 @@ uint8_t TktlGetLPCFrame::ReverseBits(uint8_t byte) {
      	return r >> 24;
      }
 };
+
 
 
 
--- alex/objects/tktl/voice/talker_tools/inc/lpc_stream.h
+++ alex/objects/tktl/voice/talker_tools/inc/lpc_stream.h
@@ -1,20 +1,29 @@
 //
-//  lpc_stream.hpp
-//  C++ Tests
+//  lpc_stream.cpp
 //
 //  Created by alx on 21/03/2018.
 //  Copyright © 2018 alx. All rights reserved.
 //
+//  C++ implementation adapted from Talkie library for Arduino by Peter Knight
+//  https://github.com/going-digital/Talkie
+//  With huge thanks!
+//
 
-#ifndef LPC_STREAM_H
-#define LPC_STREAM_H
+#ifndef TKTL_LPC_STREAM_H
+#define TKTL_LPC_STREAM_H
 
-/////////////////
-// LPC indices //
-/////////////////
+/////////////////////
+/////////////////////
+//// LPC indices ////
+/////////////////////
+/////////////////////
 
 class TktlLpcIndices {
 public:
+	/////////////////////////
+	// Public data members //
+	/////////////////////////
+	
     uint8_t indices_[12];
     /*
          LPC Indices array:
@@ -31,20 +40,39 @@ public:
          10 k9
          11 k10
      */
-    // Constructor
+    
+    // Flags
+    bool is_reading_ ;
+	bool word_end_trigger_;
+	bool frame_trigger_ ;
+	bool is_unvoiced_frame_;
+	bool is_rest_frame_;
+	bool is_repeat_frame_;
+
+    ////////////////////
+    // Public methods //
+    ////////////////////
+	
+	// Constructor
     TktlLpcIndices();
     // Zero all K values
-    void Silence();
+    void SetSilent();
     // Zero K values for unvoiced frame
-    void Unvoiced();
+    void SetUnvoiced();
 };
 
-/////////////////////////////
-// Point in LPC datastream //
-/////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+//// Point in LPC datastream ////
+/////////////////////////////////
+/////////////////////////////////
 
 class TktlLpcStreamPosition {
 public:
+	/////////////////////////
+	// Public data members //
+	/////////////////////////
+	
     uint8_t *word_start_byte_;
     
     uint8_t *byte_;
@@ -55,85 +83,142 @@ public:
     uint8_t saved_bit_;
     uint8_t saved_fragment_;	// (Currently unused)
     
+    ////////////////////
+    // Public methods //
+    ////////////////////
+    
     // Constructor
     TktlLpcStreamPosition();
     
-    // Set to playhead to word-start
-    void SetWordStart(uint8_t *byte);
+	// Set new word-start position
+	void SetWordStart(uint8_t *byte);
+
+	// Jump playhead to word-start
+	void SetToWordStart();
+
+	// Set playhead to arbitrary point
+	void SetToPosition(
+		uint8_t *byte,
+		uint8_t bit,
+		uint8_t fragment
+	);
+
+	// Set saved position
+	void SetSavedPosition();
+
+	// (Re)set to saved position
+	void SetToSavedPosition();
     
-    // Reset to start of word (ie for loop)
-    void ResetToWordStart();
-    
-    // Set playhead to point
-    void SetToPosition(
-    	uint8_t *byte,
-    	uint8_t bit,
-    	uint8_t fragment
-    );
-    
-    // Set saved position
-    void SetSavedPosition();
-    
-    // (Re)set to saved position
-    void SetToSavedPosition();
 };
 
-////////////////////////////////////
-// Get frame from LPC data stream //
-////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+//// Get frame from LPC data stream ////
+////////////////////////////////////////
+////////////////////////////////////////
 
 class TktlGetLPCFrame {
 public:
+	////////////////////
+	// Public methods //
+	////////////////////
+    
     // Constructor
     TktlGetLPCFrame();
     
-     // Get frame
-    TktlLpcIndices GetFrame();
-    
-    // Set word-start pointer
-    void StartWord(uint8_t *ptr);
-    
-    // Set loop
-    void SetLoop(bool loop);
-    
-    // Set glitch
-    void SetGlitch(bool glitch);
-    
-    // Stop reading frame data
-    void StopRead();
-    
-    // Check for word-end
-    bool CheckForWordEnd();
-    
-    // Set jump-point
-    void SetJumpPoint();
-    
-    // Set play-head to jump-point
-    void SetToJumpPoint();
+	// Set pitch bit count
+	void SetPitchBits(uint8_t bits);
+
+	// Set gate on/off
+	// oneshot flag disables gate if set true)
+	void SetGate(bool gate, bool oneshot);
+
+	// Set word start pointer to start of word LPC data
+	void SetWordStart(uint8_t *ptr);
+
+	// Begin playback from start of selected word
+	void StartReading();
+	
+	// Stop reading frame data
+	void StopReading();
+
+	// Set loop on/off
+	void SetLoop(bool loop);
+
+	// Set glitch on/off
+	void SetGlitch(bool glitch);
+
+	// Set freeze on/off
+	void SetFreeze(bool freeze);
+
+	// Step to next frame when in freeze mode
+	void StepFrozenFrame();
+
+	// Set new jump-point
+	void SetJumpPoint();
+	
+	// Jump play-head to previously-set jump-point
+	void SetToJumpPoint();
+
+	// Set frame length (determines playback rate)
+	void SetFrameLength(uint32_t length);
+
+	// Tick object (call every K-Rate cycle)
+	TktlLpcIndices Tick();
     
 private:
+	//////////////////////////
+	// Private data members //
+	//////////////////////////
+	
+	// Pointer to start of word
     uint8_t *ptr_word_start_;
+    
+    // Word-playback flags
+    bool word_changed_;
+    bool gate_;
     bool read_word_;
-    bool frame_loop_;
-    bool frame_glitch_;
+    bool freeze_;
+    bool loop_;
+	bool glitch_;
+    
+    // Global LPC settings
+    uint8_t pitch_bits_;
+    
+    // Per-frame LPC settings
     uint8_t frame_repeat_;
     uint8_t frame_energy_;
-    bool word_end_;
     
+    // Word-playback settings
+    bool word_end_;   
+	uint32_t counter_;
+	uint8_t frame_length_ ;
+	bool step_frame_;
+    
+    // Stream position object instance
     TktlLpcStreamPosition head_;
+    
+    // LPC indices object instance
     TktlLpcIndices lpc_indices_;
     
-    // Read LPC frame, updating index values in 'lpc_indices'
-    void ReadFrame();
+    /////////////////////
+    // Private Methods //
+    /////////////////////
     
     // Read bits from ROM
     uint8_t GetBits(uint8_t bits);
     
     // Reverse bits
     uint8_t ReverseBits(uint8_t byte);
+    
+    // Read LPC frame, updating index values in 'lpc_indices'
+    void ReadFrame();
+    
+    // TODO
+    void IncrementCounters();
 };
 
 #include "lpc_stream.cpp"
 
-#endif /* LPC_STREAM_H */
+#endif /* TKTL_LPC_STREAM_H */
 
