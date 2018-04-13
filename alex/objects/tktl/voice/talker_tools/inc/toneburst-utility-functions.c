c60c994964ba9c4bba307d14e0311a7aef092df5
Alex <a.drinkwater@gold.ac.uk>
2018-04-13 12:54:32 +0100

    ALEX: Updated FF Reader, Util Functions
    Added Word-End pulse outlet to FF Reader.
    Added momentary glitch effect to FF Reader.
    
    Updated toneburst functions file with Simon's fixed-point versions.

--- alex/objects/tktl/voice/talker_tools/inc/toneburst-utility-functions.c
+++ alex/objects/tktl/voice/talker_tools/inc/toneburst-utility-functions.c
@@ -1,17 +1,17 @@
-//// 8-bit max
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Max8(int8_t val, int8_t max) {
-//	return (val <= max) ? val : max;
-//};
+// 8-bit max
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Max8(int8_t val, int8_t max) {
+	return (val <= max) ? val : max;
+};
 
-//// 16-bit max
+// 16-bit max
 __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Max16(int16_t val, int16_t max) {
 	return (val <= max) ? val : max;
 };
 
-//// 32-bit max
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Max32(int32_t val, int32_t max) {
-//	return (val <= max) ? val : max;
-//};
+// 32-bit max
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Max32(int32_t val, int32_t max) {
+	return (val <= max) ? val : max;
+};
 
 //// 8-bit min
 __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Min8(int8_t val, int8_t min) {
@@ -28,42 +28,39 @@ __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Min32(int32_t val, i
 	return (val >= min) ? val : min;
 };
 
-//// 8-Bit clamp val to range min > max
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int8_t Clamp8(int8_t val, int8_t min, int8_t max) {
-//	return (val <= min) ? min : (val >= max) ? max : val;
-//};
+// 8-Bit clamp val to range min > max
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int8_t Clamp8(int8_t val, int8_t min, int8_t max) {
+	return (val <= min) ? min : (val >= max) ? max : val;
+};
 
-//// 16-Bit clamp val to range min > max
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int16_t Clamp16(int16_t val, int16_t min, int16_t max) {
-//	return (val <= min) ? min : (val >= max) ? max : val;
-//};
+// 16-Bit clamp val to range min > max
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int16_t Clamp16(int16_t val, int16_t min, int16_t max) {
+	return (val <= min) ? min : (val >= max) ? max : val;
+};
 
-//// 32-Bit clamp val to range min > max
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Clamp32(int32_t val, int32_t min, int32_t max) {
-//	return (val <= min) ? min : (val >= max) ? max : val;
-//};
+// 32-Bit clamp val to range min > max
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Clamp32(int32_t val, int32_t min, int32_t max) {
+	return (val <= min) ? min : (val >= max) ? max : val;
+};
 
-//// Scale positive input to range out_min > out_max (doesn't work with signed ints?)
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t MapU32(uint32_t val, uint32_t out_min, uint32_t out_max) {
-//	// Convert input vals to float
-//	float x = (float)val;
-//	float outMin = (float)out_min;
-//	float outMax = (float)out_max;
-//	// Return val in correct 27-bit integer range
-//	return (int32_t)(x * (outMax - outMin) / 0x07FFFFFF + outMin);
-//};
+// Scale positive input to range out_min > out_max (doesn't work with signed ints?)
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t MapU32(uint32_t val, uint32_t out_min, uint32_t out_max) {
+	// Convert input vals to float
+	float x = (float)val;
+	float outMin = (float)out_min;
+	float outMax = (float)out_max;
+	// Return val in correct 27-bit integer range
+	return (int32_t)(x * (outMax - outMin) / 0x07FFFFFF + outMin);
+};
 
 // Scale input to 0 > out_max range
 __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t ScaleU32(int32_t val, int32_t out_max) {
-	int64_t a = (int64_t)out_max * val;
-    return a >> 27;
+	return (val * out_max / 0x07FFFFFF);
 };
 
-// Map value between min and max. Taken from TSG/math/map object by Johannes Elliesen
-__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t MapU32(int32_t val, int32_t out_min, int32_t out_max) {
-	int64_t a = (int64_t)out_max * val;
-	a += (int64_t)out_min * ((128 << 20) - val);
-	return a >> 27;
+// Map value between min and max. Rewritten by lokki
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t MapU32(int64_t val, int32_t out_min, int32_t out_max) {
+	return (((val * (out_max - out_min)) / 0x07FFFFFF) + out_min);
 };
 
 // Map value float (0.0f to 1.0f range) between min and max. 
@@ -74,20 +71,17 @@ __attribute__ ( ( always_inline ) ) __STATIC_INLINE float MapF(float val, float 
 
 // Logarithmic interpolation for 27-bit param/inlet values
 __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t CurveLog32(int32_t val) {
-	// Transform inlet val to float in 0 - 1 range
-	float inf = val * (float)(1.0f / (1 << 27));
-	float curve = 1 - (1 - inf) * (1 - inf);
-	// Return val in correct 27-bit integer range
-	return (int32_t)(curve * (float)(1 << 27));
+	// Invert range
+	int32_t inv = (1 << 27) - val;
+	//Multiply of the MSB (arm instruction)
+	int32_t curve = (1 << 27) - ___SMMUL(inv << 3, inv << 2);
+	
+	return curve;
 };
 
 // Exponential interpolation for 27-bit param/inlet values
 __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t CurveExp32(int32_t val) {
-	// Transform inlet val to float in 0 - 1 range
-	float inf = val * (float)(1.0f / (1 << 27));
-	float curve = inf*inf;
-	// Return val in correct 27-bit integer range
-	return (int32_t)(curve * (float)(1 << 27));
+	return ___SMMUL(val << 3, val << 2);
 };
 
 // 16-bit xfade
@@ -120,15 +114,15 @@ __attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Slew32(int32_t targe
 		return target;
 	};
 };
-//
-//// 32-bit square root
-//// (Adapted from Factory sqrt object)
-//__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Sqrt32(int32_t val) {
-//	int32_t ai = (val > 0) ? val : -val;
-//	float aif = ai;
-//	aif = _VSQRTF(aif);
-//	return (int)aif;
-//};
+
+// 32-bit square root
+// (Adapted from Factory sqrt object)
+__attribute__ ( ( always_inline ) ) __STATIC_INLINE int32_t Sqrt32(int32_t val) {
+	int32_t ai = (val > 0) ? val : -val;
+	float aif = ai;
+	aif = _VSQRTF(aif);
+	return (int)aif;
+};
 
 //// Check size of binary file
 //// http://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
